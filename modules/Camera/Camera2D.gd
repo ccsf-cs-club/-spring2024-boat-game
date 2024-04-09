ef3f14e15c88d69a5af896b955839ca4c5cfdd94
Vena <yppahhappy.com@gmail.com>
2024-04-08 19:26:47 -0700

    Scaled Zoom

---
+++ modules/Camera/Camera2D.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+@export var base_width : float = 1920
+
+func _ready():
+	adjust_camera_zoom()
+	# getting viewport and connecting to size_changed signal!!!
+	# Calls our function when signal emits something!
+	get_viewport().size_changed.connect(adjust_camera_zoom)
+
+func adjust_camera_zoom():
+	var current_width : float = get_viewport().size.x
+	var zoom_factor : float = current_width / base_width
+	zoom = 4 * Vector2(zoom_factor, zoom_factor)
+	print(current_width, " ", zoom)
