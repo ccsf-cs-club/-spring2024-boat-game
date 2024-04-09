422fa2c22ce38fafbadc0a5b8a22d49f8ab54ff9
Vena <yppahhappy.com@gmail.com>
2024-04-08 17:20:27 -0700

    Working on ui

--- modules/ui/welcome_screen.gd
+++ modules/ui/welcome_screen.gd
@@ -7,15 +7,16 @@ class_name WelcomeScreen
 @export var camera : Camera2D
 @export var ambient_slider : HSlider
 
+
 @export var bus_name: String = "music"
 var bus_index: int
 
+
 func _read():
 	bus_index = AudioServer.get_bus_index(bus_name)
-	ambient_slider.value_changed.connect(_on_value_changed)
 
-func _on_value_changed(value):
-	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value * 0.05))
+func _on_ambient_value_changed(value):
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 
 func _on_start_game_button_up():
 	visible = false
@@ -35,3 +36,10 @@ func _on_spin_camera_pressed():
 	camera.ignore_rotation = !camera.ignore_rotation
 
 
+
+func _input(event:InputEvent)->void:
+	print("input called!")
+	if Input.is_action_pressed("openSettings"):
+		print("Raaa")
+		main_menu.visible = !main_menu.visible
+		settings.visible = !settings.visible
